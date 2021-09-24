fe52e44eef5bd8cd8c2527f1a9608c7573d6bf8e
ahmed sayed <58530571+ahmedsayed621@users.noreply.github.com>
2021-09-25 00:39:21 +0200

    AddEntities

--- eTicketApp/Startup.cs
+++ eTicketApp/Startup.cs
@@ -1,6 +1,8 @@
+using eTicketApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +25,9 @@ namespace eTicketApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +55,11 @@ namespace eTicketApp
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
             });
+            AppDbInitializer.Seed(app);
+
         }
     }
 }
